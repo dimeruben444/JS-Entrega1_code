9165e5c13375c029a5297c10ce836d7d0ffeb0ee
Ruben <dimeruben444@gmail.com>
2026-02-12 08:41:50 +0100

    feat(1,2,3,4,5) exercices 1-5 completed

--- src/ejercicio4.js
+++ src/ejercicio4.js
@@ -1,1 +1,48 @@
 
+
+let buscador = document.getElementById('buscar')
+let listaElements = document.querySelectorAll('#lista li')
+let lista = document.getElementById('lista')
+
+//se guardan los elementos de la lista en arrlist
+let arraylist = []
+listaElements.forEach(element => {
+    arraylist.push(element.textContent.toLowerCase())
+});
+
+
+
+
+//cada vez que se pulsa una tecla
+buscador.addEventListener('keyup',(event)=>{
+    let estan =[] //aray estan se inicia vacio
+    lista.innerHTML=''//la lista del html se vacia
+
+
+
+    //coje cada elemento de array list y comprueva si coincide con lo que ha metido el usuario y lo mete en el array estan 
+    arraylist.forEach(element => {
+        if(element.includes(buscador.value.toLowerCase().trim())){
+
+            estan.push(element)
+        } 
+        
+    });
+
+
+    //por cada elemento del array estan crea un li y lo mete en el ul 
+    estan.forEach(element => {
+        let newli= document.createElement('li')
+
+        newli.textContent = `${element.charAt(0).toUpperCase() + element.slice(1)}`;
+
+        lista.appendChild(newli)
+    });
+})
+
+
+
+
+
+
+
--- src/ejercicio5.js
+++ src/ejercicio5.js
@@ -1,1 +1,40 @@
 
+let sumar = document.getElementById('sumar')
+let restar = document.getElementById('restar')
+let multiplicar = document.getElementById('multiplicar')
+let dividir = document.getElementById('dividir')
+let result = document.getElementById('result')
+let signo = document.getElementById('signo')
+let n1= document.getElementById('n1')
+let n2= document.getElementById('n2')
+
+
+sumar.addEventListener('click',(event)=>{
+    event.preventDefault()
+    signo.textContent= '+'
+    let suma = Number(n1.value) + Number(n2.value)
+    result.value= suma
+})
+
+restar.addEventListener('click',(event)=>{
+    event.preventDefault()
+    signo.textContent= '-'
+    let resta = Number(n1.value) - Number(n2.value)
+    result.value= resta
+})
+
+
+multiplicar.addEventListener('click',(event)=>{
+    event.preventDefault()
+    signo.textContent= 'x'
+    let  multiplicar = Number(n1.value) * Number(n2.value)
+    result.value= multiplicar
+})
+
+dividir.addEventListener('click',(event)=>{
+    event.preventDefault()
+    signo.textContent= '/'
+    let division = Number(n1.value) / Number(n2.value)
+    result.value= division
+})
+
