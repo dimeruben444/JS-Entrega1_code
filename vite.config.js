0cc62c80cac065f0b8c8a50748a6ca71f764ae02
Ruben <dimeruben444@gmail.com>
2026-02-13 20:13:40 +0100

    fixed(build)dist listo para subir

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: './', // raíz del proyecto, normalmente './'
-  server: {
-    port: 5173,   // o el que uses
-    open: true,   // abre el navegador automáticamente
-    watch: {
-      usePolling: true, // útil si Hot Reload no funciona a veces
-    },
-  },
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        ejercicio1: resolve(__dirname, 'ejercicio1.html'),
+        ejercicio2: resolve(__dirname, 'ejercicio2.html'),
+        ejercicio3: resolve(__dirname, 'ejercicio3.html'),
+        ejercicio4: resolve(__dirname, 'ejercicio4.html'),
+        ejercicio5: resolve(__dirname, 'ejercicio5.html'),
+        ejercicio6: resolve(__dirname, 'ejercicio6.html'),
+        ejercicio7: resolve(__dirname, 'ejercicio7.html'),
+        ejercicio8: resolve(__dirname, 'ejercicio8.html'),
+        ejercicio9: resolve(__dirname, 'ejercicio9.html'),
+      }
+    }
+  }
 });
